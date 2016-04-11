392fc3774e524f31ede675a8590887b84635279f
Andy Royle <ajroyle@gmail.com>
2016-04-11 09:47:58 +0100

    include namespace and topic name in the output filename

--- zabbix-sender.sh
+++ zabbix-sender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dirBase="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-datasend_zabbix="$dirBase/DataSend_zabbix.conf"
+datasend_zabbix="$dirBase/dataSend_zabbix_$2_$4.conf"
 topicpercent=`$dirBase/azure-sb.py -h $2 -k $3 -a topic-percent -t $4`
 hostname="$2.servicebus.windows.net"
 
